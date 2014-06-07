cc8dc9b01089d104cf6155b341963ea9736b3a75
danx0r <danbmil99@gmail.com>
2014-06-07 12:52:32 -0700

    so simple it hurts

--- Dot.py
+++ Dot.py
@@ -7,20 +7,23 @@ class Dot(dict):
     def __setattr__(self, key, val):
         self[key] = val
 
+def select(gen):
+    return list(gen)
+
 if __name__ == "__main__":
     foo = Dot()
     foo['xyz'] = 456
     print foo.xyz
     foo.abc = 123
     print foo['abc']
-    print "> 200:", [e for e in foo if foo[e] > 200]
-    print "> 100:", [e for e in foo if foo[e] > 100]
+    print "> 200:", select(e for e in foo if foo[e] > 200)
+    print "> 100:", select(e for e in foo if foo[e] > 100)
     foo = Dot()
     foo.dee = Dot(bar = 11111)
     foo['def'] = Dot({'bar': 11112})         #foo.def errors due to keyword
     print foo
-    print "nested:",[e for e in foo if foo[e].bar > 11112]#foo.GE.bar(11112)
-    x = [e for e in foo if foo[e].bar == 11111][0]
+    print "nested:",select(e for e in foo if foo[e].bar > 11112)
+    x = select(e for e in foo if foo[e].bar == 11111)[0]
     print x, foo[x].bar
-    print "pony style:", [e for e in foo if foo[e].bar > 11110 and foo[e].bar < 11112]
+    print select(e for e in foo if foo[e].bar > 11110 and foo[e].bar < 11112)
     
