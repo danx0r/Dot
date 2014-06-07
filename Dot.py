8928b2f57fb19a30c79dcb1a747e23746e4cc551
danx0r <danbmil99@gmail.com>
2014-06-07 12:46:25 -0700

    clarity

--- Dot.py
+++ Dot.py
@@ -97,12 +97,12 @@ if __name__ == "__main__":
     print foo.xyz
     foo.abc = 123
     print foo['abc']
+    print "> 200:", [e for e in foo if foo[e] > 200]
+    print "> 100:", [e for e in foo if foo[e] > 100]
     foo = Dot()
     foo.dee = Dot(bar = 11111)
     foo['def'] = Dot({'bar': 11112})         #foo.def errors due to keyword
     print foo
-    print "> 200:", [e for e in foo if foo[e] > 200]
-    print "> 100:", [e for e in foo if foo[e] > 100]
     print "nested:",[e for e in foo if foo[e].bar > 11112]#foo.GE.bar(11112)
     x = [e for e in foo if foo[e].bar == 11111][0]
     print x, foo[x].bar
